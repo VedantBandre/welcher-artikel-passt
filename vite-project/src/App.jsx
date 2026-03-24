fd1100654a90107a00ff371639d8483cb0a66338
VedantBandre <vedantbandre33@gmail.com>
2026-03-24 18:33:43 +0100

    initial structure

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { GameHeader } from "./components/GameHeader";
+import { Card } from "./components/Card";
+import { WinMessage } from "./components/WinMessage";
+import { useGameLogic } from "./hooks/useGameLogic";
+
+// 8 different things, pasted twice
+const cardValues = [
+  "Brot",
+  "Reis",
+  "Wasser",
+  "Saft",
+  "Nudeln",
+  "Pizza",
+  "Croissant",
+  "Suppe",
+  "Brot",
+  "Reis",
+  "Wasser",
+  "Saft",
+  "Nudeln",
+  "Pizza",
+  "Croissant",
+  "Suppe"
+];
+
+function App() {
+  const {cards, score, moves, handleCardClick, initializeGame, isGameComplete} = useGameLogic(cardValues)
+
+  return (
+  <div className="app">
+    <GameHeader score={score} moves={moves} onReset={initializeGame}/>
+
+    {isGameComplete && <WinMessage moves = {moves}/>}
+
+    <div className="cards-grid">
+      {cardValues.map((card) => (
+        <Card card={card} onClick={() => handleCardClick(card)} key = {card.id}/>
+      ))}
+    </div>
+  </div>
+  );
+
+  // return <>Hello World!</>;
+
+}
+
+
+export default App;
