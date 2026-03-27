3f9422af53140c1b599b4874d77b49c387c176b1
VedantBandre <vedantbandre33@gmail.com>
2026-03-27 17:00:53 +0100

    basic css

--- vite-project/src/components/Game.jsx
+++ vite-project/src/components/Game.jsx
@@ -8,19 +8,23 @@ const Game = () => {
     }
 
     return (
-        <div>
-            <h1>Artikel raten</h1>
+        <div style={{ paddingTop:'3.5rem', display:'flex', flexDirection:'column', alignItems:'center', gap:'1rem', fontSize: '1.25rem'}}>
+            <h1>Welcher Artikel passt?</h1>
 
-            <h2>{currentWord.word}</h2>
+            <h2 style={{ fontSize: '3rem' }}>{currentWord.word}</h2>
 
-            <button onClick={() => checkAnswer("der")}>der</button>
-            <button onClick={() => checkAnswer("die")}>die</button>
-            <button onClick={() => checkAnswer("das")}>das</button>
+            <div style={{ display:'flex', gap:'1rem' }}>
+                <button style={{ background:'#00bbff' }} onClick={() => checkAnswer("der")}>der</button>
+                <button style={{ background:'#ff4a4a' }} onClick={() => checkAnswer("die")}>die</button>
+                <button style={{ background:'#3ef78e' }} onClick={() => checkAnswer("das")}>das</button>
+            </div>
 
-            {isCorrect === true && <p>Richtig!</p>}
-            {isCorrect === false && <p>Falsch!</p>}
+            <div style={{ height:'1.5rem' }}>
+                {isCorrect === true && <p style={{ color:'green' }}>Richtig!</p>}
+                {isCorrect === false && <p style={{ color:'red' }}>Falsch!</p>}
+            </div>
 
-            <p>Score: {score}</p>
+            <p style={{ fontSize: '2rem' }}>Score: {score}</p>
         </div>
     );
 };
