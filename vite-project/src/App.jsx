7d0b20ea4f6682182f722136c812f3d2357503c6
VedantBandre <vedantbandre33@gmail.com>
2026-03-27 16:02:49 +0100

    better logic

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,53 +1,11 @@
-import { useEffect, useState } from "react";
-import { GameHeader } from "./components/GameHeader";
-import { Card } from "./components/Card";
-import { WinMessage } from "./components/WinMessage";
-import { useGameLogic } from "./hooks/useGameLogic";
-
-// 8 different things, pasted twice
-const cardValues = [
-  "Brot",
-  "Reis",
-  "Wasser",
-  "Saft",
-  "Nudeln",
-  "Pizza",
-  "Croissant",
-  "Suppe",
-  "Brot",
-  "Reis",
-  "Wasser",
-  "Saft",
-  "Nudeln",
-  "Pizza",
-  "Croissant",
-  "Suppe"
-];
+import Game from "./components/Game";
 
 function App() {
-  const {cards, score, moves, handleCardClick, initializeGame, isGameComplete} = useGameLogic(cardValues)
-
-  return (
-  <div className="app">
-    <GameHeader score={score} moves={moves} onReset={initializeGame}/>
-
-    {isGameComplete && <WinMessage moves = {moves}/>}
-
-    <div className="cards-grid">
-      {cards.map((card) => (
-        <Card 
-          key = {card.id}  
-          card={card} 
-          onClick={() => handleCardClick(card)} 
-        />
-      ))}
-    </div>
-  </div>
-  );
-
-  // return <>Hello World!</>;
-
+    return (
+        <div>
+            <Game />
+        </div>
+    );
 }
-
 
 export default App;
